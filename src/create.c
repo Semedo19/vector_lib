b816dfaaa5b3ac3adc3aca1fbf7bcf2f32ada5c8
Thiago Semedo-Rocha <thiago.semedo-rocha@epitech.eu>
2025-12-15 17:50:22 +0100

    refactor: create no longer casting while returning

--- src/create.c
+++ src/create.c
@@ -21,5 +21,5 @@ void *vector_create(size_t data_size, size_t initial_capacity)
     metadata->data_size = data_size;
     metadata->capacity = initial_capacity;
     metadata->length = 0;
-    return (void *)(get_vector(metadata));
+    return get_vector(metadata);
 }
